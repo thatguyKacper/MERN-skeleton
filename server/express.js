c8a26ad1e69383aac4be21420c16380c90dd25e9
Kacper <kacper.m.wojtowicz@gmail.com>
2022-06-17 10:59:25 +0200

    deploy

--- server/express.js
+++ server/express.js
@@ -19,14 +19,14 @@ import { ServerStyleSheets, ThemeProvider } from '@material-ui/styles';
 import theme from './../client/theme';
 
 //comment out before building for production
-import devBundle from './devBundle';
+// import devBundle from './devBundle';
 
 const CURRENT_WORKING_DIR = process.cwd();
 
 const app = express();
 
 //comment out before building for production
-devBundle.compile(app);
+// devBundle.compile(app);
 
 // parse body params and attache them to req.body
 app.use(bodyParser.json());
